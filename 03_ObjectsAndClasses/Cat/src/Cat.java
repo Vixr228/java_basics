a493cd27dfe1502e8ab92b553f0646bad352374b
kostya <kostya.krsk@gmail.com>
2020-10-29 19:35:39 +0700

    Task 2 done

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -7,12 +7,15 @@ public class Cat
     private double minWeight;
     private double maxWeight;
 
+    private double foodAmount;
+
     public Cat()
     {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        foodAmount = 0;
 
     }
 
@@ -22,9 +25,22 @@ public class Cat
         System.out.println("Meow");
     }
 
+    /** Создайте в классе Cat метод, который возвращает сумму съеденной еды текущей кошки. */
+
+    public double getFoodAmount(){
+        return foodAmount;
+    }
+    /** Создайте в классе Cat метод «Сходить в туалет» pee(), который уменьшает вес кошки и что-нибудь печатает. */
+
+    public void pee(){
+        weight -= 100;
+        System.out.println("Congratulations!");
+    }
+
     public void feed(Double amount)
     {
         weight = weight + amount;
+        foodAmount += amount;
     }
 
     public void drink(Double amount)
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,39 +3,54 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat1 = new Cat();
-        Cat cat2 = new Cat();
-        Cat cat3 = new Cat();
-        Cat cat4 = new Cat();
-        Cat cat5 = new Cat();
 
-        System.out.println("Cat1: \n Status: " + cat1.getStatus() + "\n Weight: " + cat1.getWeight());
-        System.out.println("Cat2: \n Status: " + cat2.getStatus() + "\n Weight: " + cat2.getWeight());
-        System.out.println("Cat3: \n Status: " + cat3.getStatus() + "\n Weight: " + cat3.getWeight());
-        System.out.println("Cat4: \n Status: " + cat4.getStatus() + "\n Weight: " + cat4.getWeight());
-        System.out.println("Cat5: \n Status: " + cat5.getStatus() + "\n Weight: " + cat5.getWeight());
+        // TASK 1
 
+//        Cat cat1 = new Cat();
+//        Cat cat2 = new Cat();
+//        Cat cat3 = new Cat();
+//        Cat cat4 = new Cat();
+//        Cat cat5 = new Cat();
+//
+//        System.out.println("Cat1: \n Status: " + cat1.getStatus() + "\n Weight: " + cat1.getWeight());
+//        System.out.println("Cat2: \n Status: " + cat2.getStatus() + "\n Weight: " + cat2.getWeight());
+//        System.out.println("Cat3: \n Status: " + cat3.getStatus() + "\n Weight: " + cat3.getWeight());
+//        System.out.println("Cat4: \n Status: " + cat4.getStatus() + "\n Weight: " + cat4.getWeight());
+//        System.out.println("Cat5: \n Status: " + cat5.getStatus() + "\n Weight: " + cat5.getWeight());
+//
+//
+//        cat1.feed(21.);
+//        cat2.feed((double) 100);
+//        cat3.drink(100.);
+//
+//        System.out.println("Cat1: \n Status: " + cat1.getStatus() + "\n Weight: " + cat1.getWeight());
+//        System.out.println("Cat2: \n Status: " + cat2.getStatus() + "\n Weight: " + cat2.getWeight());
+//        System.out.println("Cat3: \n Status: " + cat3.getStatus() + "\n Weight: " + cat3.getWeight());
+//
+//
+//        System.out.println("Let's kill cat1 :(");
+//        while(cat1.getStatus() != "Exploded"){
+//            cat1.feed(1000.);
+//        }
+//        System.out.println("Cat1: \n Status: " + cat1.getStatus() + "\n Weight: " + cat1.getWeight());
+//
+//        System.out.println("Let's make a deadly meow ");
+//        while(cat2.getStatus() != "Dead"){
+//            cat2.meow();
+//        }
+//        System.out.println("Cat2: \n Status: " + cat2.getStatus() + "\n Weight: " + cat2.getWeight());
 
-        cat1.feed(21.);
-        cat2.feed((double) 100);
-        cat3.drink(100.);
+        //TASK 2
 
-        System.out.println("Cat1: \n Status: " + cat1.getStatus() + "\n Weight: " + cat1.getWeight());
-        System.out.println("Cat2: \n Status: " + cat2.getStatus() + "\n Weight: " + cat2.getWeight());
-        System.out.println("Cat3: \n Status: " + cat3.getStatus() + "\n Weight: " + cat3.getWeight());
-
-
-        System.out.println("Let's kill cat1 :(");
-        while(cat1.getStatus() != "Exploded"){
-            cat1.feed(1000.);
-        }
-        System.out.println("Cat1: \n Status: " + cat1.getStatus() + "\n Weight: " + cat1.getWeight());
-
-        System.out.println("Let's make a deadly meow ");
-        while(cat2.getStatus() != "Dead"){
-            cat2.meow();
-        }
-        System.out.println("Cat2: \n Status: " + cat2.getStatus() + "\n Weight: " + cat2.getWeight());
+        Cat cat6 = new Cat();
+        System.out.println("Cat6: \n Weight: " + cat6.getWeight());
+        cat6.feed(100.);
+        cat6.feed(150.);
+        System.out.println("Cat6: \n Weight: " + cat6.getWeight());
+        cat6.pee();
+        cat6.pee();
+        System.out.println("Cat6: \n Weight: " + cat6.getWeight());
+        System.out.println("Cat ate: " + cat6.getFoodAmount() + "g");
 
 
     }
