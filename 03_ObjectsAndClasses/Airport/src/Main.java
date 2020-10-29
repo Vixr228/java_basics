ff934091c934ca7276b4f242c287706ef3537cd8
kostya <kostya.krsk@gmail.com>
2020-10-29 23:46:44 +0700

    Task 8 done

--- 03_ObjectsAndClasses/Airport/src/Main.java
+++ 03_ObjectsAndClasses/Airport/src/Main.java
@@ -3,5 +3,10 @@ import com.skillbox.airport.Airport;
 public class Main {
     public static void main(String[] args) {
 
+        Airport airport = Airport.getInstance();
+        System.out.println(airport.getAllAircrafts().toString());
+        int airportSize = airport.getAllAircrafts().size();
+        System.out.println("Size of airport: " + airportSize);
+        //System.out.println(airport.getTerminals().toString());
     }
 }
