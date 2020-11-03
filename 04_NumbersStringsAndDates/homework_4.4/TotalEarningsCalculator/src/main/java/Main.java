b5f4af439b9468438fcfc8adceae8bfe06f87270
kostya <kostya.krsk@gmail.com>
2020-11-03 16:14:51 +0700

    4.4.2 TotalEarningCalculator done

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        //TODO: напишите ваш код, результат вывести в консоль
+
+
+        int vasyaSalary = Integer.parseInt(text.substring(text.indexOf('5'), text.indexOf('5') + 4));
+        int petyaSalary = Integer.parseInt(text.substring(text.indexOf('7'), text.indexOf('7') + 4));
+        int mashaSalary = Integer.parseInt(text.substring(text.lastIndexOf('3'), text.lastIndexOf('3') + 5));
+//        System.out.println("Зарплата Василия: " + vasyaSalary);
+//        System.out.println("Зарплата Петра: " + petyaSalary);
+//        System.out.println("Зарплата Марии: " + mashaSalary);
+//        System.out.println("Сумма заработка: " + (vasyaSalary + mashaSalary + petyaSalary));
+        System.out.println((vasyaSalary + mashaSalary + petyaSalary));
     }
 
 }
