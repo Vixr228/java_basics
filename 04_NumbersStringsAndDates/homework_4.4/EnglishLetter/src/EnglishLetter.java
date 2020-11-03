b216162348bc19de0505a410fead6cf3f4d7e6f5
kostya <kostya.krsk@gmail.com>
2020-11-03 15:50:07 +0700

    4.4.1 EnglishLetter done

--- 04_NumbersStringsAndDates/homework_4.4/EnglishLetter/src/EnglishLetter.java
+++ 04_NumbersStringsAndDates/homework_4.4/EnglishLetter/src/EnglishLetter.java
@@ -1,6 +1,8 @@
 public class EnglishLetter {
     public static void main(String[] args) {
         // Напишите код, который распечатает коды всех букв английского алфавита и сами буквы.
-        System.out.println((int) 'A');
+        for (int i = 'A'; i <= 'Z'; i++) {
+            System.out.println((char) i + " - " + i);
+        }
     }
 }
