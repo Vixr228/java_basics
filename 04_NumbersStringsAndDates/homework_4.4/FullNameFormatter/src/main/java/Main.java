7924cf1e84406bd14d4fbbd7c509a7748e2c4448
kostya <kostya.krsk@gmail.com>
2020-11-04 18:35:01 +0700

    4.4.3 FullNameFormater done!

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -4,15 +4,53 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String name = " ";
+        String surname = " ";
+        String fatherName = " ";
+
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            //TODO:напишите ваш код тут, результат вывести в консоль.
-            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+            boolean checkFlag = true;
+            //Проверка на кирилицу
+            for (int i = 0; i < input.length(); i++) {
+                if ((input.charAt(i) > 1040 && input.charAt(i) < 1104) || input.charAt(i) == '-' || input.charAt(i) == ' ')
+                    continue;
+                else checkFlag = false;
+
+            }
+
+            //проверка на три слова
+            int spaceCount = 0;
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == ' ') spaceCount++;
+            }
+            if (spaceCount != 2) checkFlag = false;
+
+            //Следущая попытка
+            if(!checkFlag) {
+                System.out.println("Введенная строка не является ФИО");
+                continue;
+            }
+
+
+            surname = input.substring(0, input.indexOf(' '));
+            name = input.substring(input.indexOf(' '), input.lastIndexOf(' '));
+            fatherName = input.substring(input.lastIndexOf(' '), input.length());
+
+
+            System.out.println("Фамилия: " + surname.trim());
+            System.out.println("Имя: " + name.trim());
+            System.out.println("Отчество: " + fatherName.trim());
         }
+
+
     }
+
 
 }
