bca444652248a4882fa6f1a2dab147d1213dcce2
kostya <kostya.krsk@gmail.com>
2020-11-09 19:49:18 +0700

    4.5.1 EarningsCalculatorReqExp not good, but works.

--- 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/MaskingInformation/src/main/java/Main.java
@@ -1,15 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008> 1234 <5678> 8912", "***");
-        System.out.println(safe);
+//        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008> 1234 <5678> 8912", "***");
+//        System.out.println(safe);
+        String str = "Номер кредитной карты <4008> 1234";
+        String placeholder = "***";
+
+
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         String textTemp = text;
         while (textTemp.indexOf('<') != -1 && textTemp.indexOf('>') != -1) {
             textTemp = textTemp.substring(0, textTemp.indexOf('<')) +
-                    placeholder + textTemp.substring(textTemp.indexOf('>') + 1, textTemp.length());
+                    placeholder + textTemp.substring(textTemp.indexOf('>') + 1);
         }
         return textTemp;
     }
--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -2,11 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(calculateSalarySum("Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей"));
+
     }
 
     public static int calculateSalarySum(String text) {
         //TODO: реализуйте метод
-        return -1;
+        text = text.replaceAll("[^\\d\\s]", "");
+        String res[] = text.split("\\s+");
+//        for (int i = 0; i < res.length; i++) {
+//            System.out.println(res[i]);
+//        }
+        int sum = 0;
+        for (int i = 1; i < res.length; i++) {
+            sum += Integer.parseInt(res[i].trim());
+        }
+        return sum;
     }
 
 }
