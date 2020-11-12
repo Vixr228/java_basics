8e3883ea8d5386d839798f7c1b47361e100b3856
kostya <kostya.krsk@gmail.com>
2020-11-12 17:07:03 +0700

    4.4 TotalEarningsCalculator fixed

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -5,13 +5,23 @@ public class Main {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
 
-        int vasyaSalary = Integer.parseInt(text.substring(text.indexOf('5'), text.indexOf('5') + 4));
-        int petyaSalary = Integer.parseInt(text.substring(text.indexOf('7'), text.indexOf('7') + 4));
-        int mashaSalary = Integer.parseInt(text.substring(text.lastIndexOf('3'), text.lastIndexOf('3') + 5));
+
+        String vasyaSalSubstr=text.substring(0,text.indexOf(" руб"));
+        String vasyaSalStr=vasyaSalSubstr.substring(vasyaSalSubstr.lastIndexOf(' ')).trim();
+        int vasyaSalary=Integer.parseInt(vasyaSalStr);
+
+        String mashaSalSubst = text.substring(0, text.lastIndexOf(" руб"));
+        String mashaSalStr = mashaSalSubst.substring(mashaSalSubst.lastIndexOf(' ')).trim();
+        int mashaSalary = Integer.parseInt(mashaSalStr);
+
+        String petyaSalSubstr = text.substring(text.indexOf("руб"), text.lastIndexOf("руб"));
+        petyaSalSubstr = petyaSalSubstr.substring(0,petyaSalSubstr.lastIndexOf(" руб"));
+        String petyaSalStr = petyaSalSubstr.substring(petyaSalSubstr.lastIndexOf(' ')).trim();
+        int petyaSalary = Integer.parseInt(petyaSalStr);
+//
 //        System.out.println("Зарплата Василия: " + vasyaSalary);
 //        System.out.println("Зарплата Петра: " + petyaSalary);
 //        System.out.println("Зарплата Марии: " + mashaSalary);
-//        System.out.println("Сумма заработка: " + (vasyaSalary + mashaSalary + petyaSalary));
         System.out.println((vasyaSalary + mashaSalary + petyaSalary));
     }
 
