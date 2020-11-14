0c32ebfb466b7a6d59a700c7cb19b723b145a47c
kostya <kostya.krsk@gmail.com>
2020-11-14 12:38:52 +0700

    4.5 SplitText need fix separator.

--- 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/SplitText/src/main/java/Main.java
@@ -1,12 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        String text = "the level of lifespan-is-kek higher than it was 40 years ago.";
+        System.out.println(splitTextInToWords(text));
     }
 
     public static String splitTextInToWords(String text) {
-        //TODO реализуйте метод
-        return "";
+        String res = text.replaceAll("[0-9\\,\\.\\?\\!\\:\\;]", "");
+        res = res.replaceAll("\\s\\s", " ");
+        res = res.replaceAll("[\\s\\-]", "\n");
+        return res;
+
     }
 
 }
