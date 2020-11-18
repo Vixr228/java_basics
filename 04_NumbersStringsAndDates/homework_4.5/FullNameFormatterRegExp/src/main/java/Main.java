93719452e0240f97bbbf56322c2affaf28ee31f5
kostya <kostya.krsk@gmail.com>
2020-11-18 17:16:05 +0700

    4.5 TestNameFormatter fixed 4.5 MaskingInformationRegExp done!

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -13,16 +13,20 @@ public class Main {
                 break;
             }
             //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-            if(!input.matches("[а-яёА-ЯЁ\\-\\s]+")) {
+            if(!input.matches("[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+")) {
                 System.out.println("Введенная строка не является ФИО");
                 continue;
             }
-            String fio[] = input.split("\\s+", 3);
+            String fio[] = input.split("\\s", 3);
+//            if(fio.length < 3) {
+//                System.out.println("Введенная строка не является ФИО");
+//                continue;
+//            }
             System.out.println("Фамилия: " + fio[0]);
             System.out.println("Имя: " + fio[1]);
             System.out.println("Отчество: " + fio[2]);
 
-        //TODO Одно слово когда, то не работает
+
         }
     }
 
