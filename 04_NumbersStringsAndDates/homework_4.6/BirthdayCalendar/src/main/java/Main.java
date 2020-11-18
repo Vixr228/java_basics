6f26368ae7e168b42541b3f2bd0edda8f5a0d09c
kostya <kostya.krsk@gmail.com>
2020-11-18 19:45:41 +0700

    4.6 BirthdayCalendar done!

--- 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
@@ -1,10 +1,15 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 18;
+        int month = 11;
+        int year = 2020;
 
         System.out.println(collectBirthdays(year, month, day));
 
@@ -15,7 +20,19 @@ public class Main {
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy - EEE");
+        Calendar calendarStart = Calendar.getInstance();
+        calendarStart.set(year, month - 1, day);
+        Calendar calendarCurr = Calendar.getInstance();
+        String result = "";
+        int i = 0;
+       while(calendarStart.getTimeInMillis() <= calendarCurr.getTimeInMillis()) {
+            result += i + " - " + dateFormat.format(calendarStart.getTime()).toString() + "\n";
+            calendarStart.add(Calendar.YEAR, 1);
+            i++;
+        }
 
-        return "";
+
+        return result;
     }
 }
