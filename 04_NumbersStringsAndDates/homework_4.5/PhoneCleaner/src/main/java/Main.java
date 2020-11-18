9d4f1d9a2205043b223004f0e69707b143092cce
kostya <kostya.krsk@gmail.com>
2020-11-18 15:38:46 +0700

    4.5 TestPhoneCleaner done!

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -11,6 +13,19 @@ public class Main {
                 break;
             }
             //TODO:напишите ваш код тут, результат вывести в консоль.
+            //if(input.matches("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$")) {
+
+
+            String number = input.replaceAll("[\\+\\-()\\s]", "");
+            if(!number.matches("(^(8|7)\\d{10}|^9\\d{9})")){
+                System.out.println("Неверный формат номера");
+                continue;
+            }
+            if(number.matches("^8\\d{10}")) number = number.replaceFirst("8", "7");
+            if(number.matches("^9\\d{9}")) System.out.println("7" + number);
+            else System.out.println(number);
+
+
         }
     }
 
