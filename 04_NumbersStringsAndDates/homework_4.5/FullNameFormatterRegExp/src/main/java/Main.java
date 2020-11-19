fe87d4af01dd7d54ab1607a994b592e166aa5ab4
kostya <kostya.krsk@gmail.com>
2020-11-19 18:59:55 +0700

    4.5 FullNameFormatter with Pattern Matcher

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+$");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -12,8 +13,10 @@ public class Main {
             if (input.equals("0")) {
                 break;
             }
+            Matcher matcher = pattern.matcher(input);
             //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-            if(!input.matches("[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+")) {
+            //if(!input.matches("[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+\\s[а-яёА-ЯЁ\\-]+")) {
+            if(!matcher.find()){
                 System.out.println("Введенная строка не является ФИО");
                 continue;
             }
