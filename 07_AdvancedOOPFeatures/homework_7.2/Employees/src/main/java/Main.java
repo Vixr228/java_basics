ec60e1783c03b9d0cb2b69f78bc9a89d99c05c4b
kostya <kostya.krsk@gmail.com>
2021-01-10 20:34:02 +0700

    7.2 make func findEmployeeWithHighestSalary

--- 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Employees/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -11,8 +13,11 @@ public class Main {
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
-        // кто пришёл в году, указанном в переменной year
-        return null;
+        Employee emp = staff.stream()
+                .filter(e -> e.getWorkStart().getYear() + 1900 == year)
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
+
+        return emp;
     }
 }
