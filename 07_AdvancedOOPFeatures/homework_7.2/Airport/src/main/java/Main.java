a498f79967bc67f5932e264556e55f49ed18a3fe
kostya <kostya.krsk@gmail.com>
2021-01-17 19:22:15 +0700

    7.2 make func findPlanesLeavingInTheNextTwoHours

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,8 +13,18 @@ public class Main {
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+        int twoHoursMillis = 7200000;
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        List<Flight> flightList = new ArrayList<>();
+
+       airport.getTerminals().stream()
+              .forEach(t -> t.getFlights().stream()
+                .filter(flight -> flight.getDate().getTime() - System.currentTimeMillis() <= twoHoursMillis)
+                      .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
+                        .forEach(f -> flightList.add(f)));
+        return flightList;
+
+    
     }
 
 }
