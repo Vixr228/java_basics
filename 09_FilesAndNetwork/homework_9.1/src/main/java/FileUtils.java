246caacbad2ac19eca98fd9f9da9a865ed577154
kostya <kostya.krsk@gmail.com>
2021-02-19 23:34:32 +0700

    9.1 calculateFolderSize ready

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,21 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        File mainFolder = new File(path);
+        if(!mainFolder.isDirectory()) {
+            System.out.println("Not a directory");
+            return 0;
+        }
+
+        File[] filesInDir = mainFolder.listFiles();
+        int result = 0;
+        for(File currFile : filesInDir){
+            if(currFile.isDirectory()){
+                result += calculateFolderSize(currFile.getPath());
+            } else result += currFile.length();
+        }
+        return result;
     }
 }
