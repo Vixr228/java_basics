45e60e886930d60d2d4ce5a91b63098639979717
kostya <kostya.krsk@gmail.com>
2021-02-23 17:33:08 +0700

    9.1 calculateFolderSize ready 2.0

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,12 +1,14 @@
 import java.io.File;
+import java.io.IOException;
 
 public class FileUtils {
 
-    public static long calculateFolderSize(String path) {
+    public static long calculateFolderSize(String path) throws IllegalArgumentException, NullPointerException, IOException {
+        if(path.equals(null))  throw new IOException("Path is null");
         File mainFolder = new File(path);
+        if(!mainFolder.exists()) throw new NullPointerException("Path doesn't exist");
         if(!mainFolder.isDirectory()) {
-            System.out.println("Not a directory");
-            return 0;
+           throw new IllegalArgumentException("It is not a directory");
         }
 
         File[] filesInDir = mainFolder.listFiles();
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,9 +1,15 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String path =  scanner.nextLine();
-        System.out.println(FileUtils.calculateFolderSize(path));
+        try {
+            System.out.println(FileUtils.calculateFolderSize(path));
+        } catch (IllegalArgumentException | IOException | NullPointerException e){
+            e.printStackTrace();
+        }
+
     }
 }
