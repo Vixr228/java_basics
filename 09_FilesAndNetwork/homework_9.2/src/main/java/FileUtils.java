d1309ead11ae21bfe28373099275ad813fa0029d
kostya <kostya.krsk@gmail.com>
2021-02-24 21:49:04 +0700

    9.2 copyFile ready!

--- 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
@@ -1,5 +1,33 @@
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IllegalArgumentException, IOException {
+        //System.out.println("Start");
+        if(sourceDirectory == null || destinationDirectory == null) throw new IOException("Null paths");
+        File source = new File(sourceDirectory);
+        if(!source.isDirectory()) throw new IllegalArgumentException("Source is not a directory");
+        File[] filesInDir = source.listFiles();
+
+
+        Path destDir = Paths.get(destinationDirectory);
+        for(File currFile : filesInDir){
+           // System.out.println(currFile.getPath());
+            if(currFile.isDirectory()){
+                Files.copy(currFile.toPath(), destDir.resolve(currFile.getName()), StandardCopyOption.REPLACE_EXISTING);
+                copyFolder(currFile.getPath(), destDir.resolve(currFile.getName()).toString());
+
+            } else {
+                Files.copy(currFile.toPath(), destDir.resolve(currFile.getName()), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+
+
     }
 }
--- 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String source = scanner.nextLine();
+        String destination = scanner.nextLine();
+        try{
+            FileUtils.copyFolder(source, destination);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+       // System.out.println("END");
     }
 }
